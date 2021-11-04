d9dee25926ad37462a3ebb7f99b88529dc25bd3b
Artur Geller <ageller@qti.qualcomm.com>
2021-11-04 20:09:43 +0000

    newsletter

---
+++ newsletter/app.js
@@ -0,0 +1,78 @@
+//jshint esversion: 6
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req, res) {
+  var firstName = req.body.fName;
+  var lastName = req.body.lName;
+  var email = req.body.email;
+
+  var data = {
+    'members': [
+      {
+      email_address: email,
+      status: 'subscribed',
+      merge_fields: {
+        FNAME: firstName,
+        LNAME: lastName
+        }
+      }
+    ],
+  }
+
+  var jsonData = JSON.stringify(data)
+
+  console.log(firstName, lastName, email);
+
+  // NOTE: The API KEY BELOW HAS BEEN DISABLED ON MAILCHIMP
+  //       AS THIS CODE WILL BE PUSHED TO PUBLIC GITHUB
+
+  var options = {
+    url: 'https://us5.api.mailchimp.com/3.0/lists/9b38f6cac3',
+    method: 'POST',
+    headers: {
+      'Authorization': "surv eb9ab09d498918456d273f781c8893d9-us5"
+    },
+    body: jsonData
+  }
+
+  request(options, function(error, response, body) {
+    if (error) {
+      console.log(error);
+      res.sendFile(__dirname + "/failure.html");
+    } else {
+      if (response.statusCode == 200) {
+        res.sendFile(__dirname + "/succuss.html");
+      } else {
+        res.sendFile(__dirname + "/failure.html");
+      }
+    }
+  });
+
+});
+
+app.post("/failure", function(req, res){
+  res.redirect("/");
+});
+
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Server is running");
+});
+
+
+// ID for List/Audience:
+// 9b38f6cac3
+//eb9ab09d498918456d273f781c8893d9-us5
